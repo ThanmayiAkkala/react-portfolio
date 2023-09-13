6267630463e0d0022ad3617962710848e7428186
thanmayi okay <akkalathanmayi@gmail.com>
2023-09-13 11:01:22 -0500

    commit check

--- src/App.js
+++ src/App.js
@@ -8,30 +8,43 @@ class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+        <Layout>
+          <Header
+            className="header-color"
+            title={
+              <Link style={{ textDecoration: 'none', color: 'white' }} to="/">
+                MyPortfolio
+              </Link>
+            }
+            scroll
+          >
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
               <Link to="/projects">Projects</Link>
               <Link to="/contact">Contact</Link>
             </Navigation>
-        </Drawer>
-        <Content>
+          </Header>
+          <Drawer
+            title={
+              <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
+                MyPortfolio
+              </Link>
+            }
+          >
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
             <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
-
+            <Main />
+          </Content>
+        </Layout>
+      </div>
     );
   }
 }
--- src/components/resume.js
+++ src/components/resume.js
@@ -4,94 +4,83 @@ import Education from './education';
 import Experience from './experience';
 import Skills from './skills';
 
-
 class Resume extends Component {
   render() {
-    return(
+    return (
       <div>
         <Grid>
           <Cell col={4}>
-            <div style={{textAlign: 'center'}}>
+            <div style={{ textAlign: 'center' }}>
               <img
                 src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
                 alt="avatar"
-                style={{height: '200px'}}
-                 />
+                style={{ height: '200px' }}
+              />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Paul Hanna</h2>
-            <h4 style={{color: 'grey'}}>Programmer</h4>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h2 style={{ paddingTop: '2em' }}>Thanmayi Akkala</h2>
+            <h4 style={{ color: 'grey' }}>Programmer</h4>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            <p>
+              Computer Science graduate student with expertise in Machine
+              Learning, Data Visualization, and Web Development. Experienced in
+              developing chatbots, deploying server-side applications, and web
+              development, complemented by a strong foundation in computer
+              science subjects. Effective team player with a passion for
+              leveraging technology to drive insights and innovation
+            </p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
             <h5>Address</h5>
-            <p>1 Hacker Way Menlo Park, 94025</p>
+            <p>702 S Ada Street 60607 Chicago Illinois</p>
             <h5>Phone</h5>
-            <p>(123) 456-7890</p>
+            <p>(312) 669-4951</p>
             <h5>Email</h5>
-            <p>someone@example.com</p>
-            <h5>Web</h5>
-            <p>mywebsite.com</p>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>takkal2@uic.edu</p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
           </Cell>
           <Cell className="resume-right-col" col={8}>
             <h2>Education</h2>
 
+            <Education
+              startYear={2023}
+              endYear={2025}
+              schoolName="University of Illinois Chicago"
+              schoolDescription="MS in Computer Science"
+            />
 
             <Education
-              startYear={2002}
-              endYear={2006}
-              schoolName="My University"
+              startYear={2007}
+              endYear={2009}
+              schoolName="My 2nd University"
               schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-               />
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
 
-               <Education
-                 startYear={2007}
-                 endYear={2009}
-                 schoolName="My 2nd University"
-                 schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                  />
-                <hr style={{borderTop: '3px solid #e22947'}} />
-
-              <h2>Experience</h2>
+            <h2>Experience</h2>
 
             <Experience
               startYear={2009}
               endYear={2012}
               jobName="First Job"
               jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-              />
+            />
 
-              <Experience
-                startYear={2012}
-                endYear={2016}
-                jobName="Second Job"
-                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                />
-              <hr style={{borderTop: '3px solid #e22947'}} />
-              <h2>Skills</h2>
-              <Skills
-                skill="javascript"
-                progress={100}
-                />
-                <Skills
-                  skill="HTML/CSS"
-                  progress={80}
-                  />
-                  <Skills
-                    skill="NodeJS"
-                    progress={50}
-                    />
-                    <Skills
-                      skill="React"
-                      progress={25}
-                      />
-
-
+            <Experience
+              startYear={2012}
+              endYear={2016}
+              jobName="Second Job"
+              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            <h2>Skills</h2>
+            <Skills skill="javascript" progress={100} />
+            <Skills skill="HTML/CSS" progress={80} />
+            <Skills skill="NodeJS" progress={50} />
+            <Skills skill="React" progress={25} />
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
